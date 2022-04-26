6a91e1cf9edd5840e62f4ae0c8e6a2ed303a6696
ChillFish8 <hburt2003@gmail.com>
2022-04-26 20:27:06 +0100

    Expose tags inner

--- src/tags/mod.rs
+++ src/tags/mod.rs
@@ -3,8 +3,8 @@ mod packs;
 mod handler;
 
 pub use handler::{from_named_flags, to_named_flags, Flag, VisibleTag};
-pub use bots::{BotTags, set_bot_tags};
-pub use packs::{PackTags, set_pack_tags};
+pub use bots::{BotTags, set_bot_tags, get_bot_tags};
+pub use packs::{PackTags, set_pack_tags, get_pack_tags};
 
 pub trait IntoFilter {
     fn into_filter(self) -> Vec<String>;
