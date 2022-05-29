7d68f946cacb7cde5425db7815a08132e463c2a6
ChillFish8 <hburt2003@gmail.com>
2022-05-29 21:25:53 +0100

    Chore<fmt>: Reformat code

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -6,13 +6,13 @@ mod timestamp;
 mod unicode_aware;
 pub mod url;
 
+pub use self::url::DiscordUrl;
 pub use bigint::JsSafeBigInt;
 pub use integer::JsSafeInt;
 pub use invite::DiscordInvite;
 pub use set::Set;
 pub use timestamp::Timestamp;
 pub use unicode_aware::NormalisingString;
-pub use self::url::DiscordUrl;
 
 #[derive(serde::Serialize, serde::Deserialize)]
 #[serde(untagged)]
