3e62d6469338d2b76f86977fefa0950a0b254b8d
ChillFish8 <hburt2003@gmail.com>
2022-05-29 21:25:40 +0100

    Fix<urls>: Add extended url validators and tighten constraints add testing

--- src/types/url.rs
+++ src/types/url.rs
@@ -1,5 +1,6 @@
 use std::borrow::Cow;
 use std::fmt::{Display, Formatter};
+use std::marker::PhantomData;
 use std::ops::Deref;
 use std::str::FromStr;
 
@@ -90,19 +91,19 @@ impl ToJSON for DiscordUrl {
 
 impl ParseFromJSON for DiscordUrl {
     fn parse_from_json(value: Option<Value>) -> ParseResult<Self> {
-        let value = value.ok_or_else(|| ParseError::custom("invalid url given"))?;
+        let value = value.ok_or_else(|| ParseError::custom("Invalid url provided."))?;
 
         if let Some(v) = value.as_str() {
             let url = Url::from_str(v)?;
 
             if !is_valid_url(&url) {
-                return Err(ParseError::custom("Invalid url provided."))
+                return Err(ParseError::custom("Invalid url provided."));
             }
 
             return Ok(Self(url));
         }
 
-        Err(ParseError::custom("invalid url given"))
+        Err(ParseError::custom("Invalid url provided."))
     }
 }
 
@@ -113,7 +114,7 @@ impl FromStr for DiscordUrl {
         let url = Url::from_str(s)?;
 
         if !is_valid_url(&url) {
-            return Err(ParseError::custom("Invalid url provided."))
+            return Err(ParseError::custom("Invalid url provided."));
         }
 
         Ok(Self(url))
@@ -136,7 +137,6 @@ impl scylla::frame::value::Value for DiscordUrl {
     }
 }
 
-
 fn is_valid_url(url: &Url) -> bool {
     (url.scheme() == "http" || url.scheme() == "https")
         && url.username() == ""
@@ -145,38 +145,127 @@ fn is_valid_url(url: &Url) -> bool {
         && url.domain().is_some()
 }
 
+#[derive(Clone, Default, PartialEq, Eq, Hash, Debug, serde::Serialize, serde::Deserialize)]
+pub struct ConstrainedDiscordUrl<T: constraints::ConstrainedUrl>(pub DiscordUrl, PhantomData<T>);
 
-#[derive(Clone, PartialEq, Eq, Hash, Debug, serde::Serialize, serde::Deserialize)]
-pub struct ConstrainedDiscordUrl(pub DiscordUrl);
+impl<T: constraints::ConstrainedUrl> From<DiscordUrl> for ConstrainedDiscordUrl<T> {
+    fn from(v: DiscordUrl) -> Self {
+        Self(v, PhantomData::default())
+    }
+}
 
+impl<T: constraints::ConstrainedUrl> Display for ConstrainedDiscordUrl<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl<T: constraints::ConstrainedUrl> Deref for ConstrainedDiscordUrl<T> {
+    type Target = DiscordUrl;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T: constraints::ConstrainedUrl + Sync + Send + 'static> Type for ConstrainedDiscordUrl<T> {
+    const IS_REQUIRED: bool = <DiscordUrl as Type>::IS_REQUIRED;
+    type RawValueType = <DiscordUrl as Type>::RawValueType;
+    type RawElementValueType = <DiscordUrl as Type>::RawElementValueType;
+
+    fn name() -> Cow<'static, str> {
+        DiscordUrl::name()
+    }
+
+    fn schema_ref() -> MetaSchemaRef {
+        DiscordUrl::schema_ref()
+    }
+
+    fn as_raw_value(&self) -> Option<&Self::RawValueType> {
+        self.0.as_raw_value()
+    }
+
+    fn raw_element_iter<'a>(
+        &'a self,
+    ) -> Box<dyn Iterator<Item = &'a Self::RawElementValueType> + 'a> {
+        self.0.raw_element_iter()
+    }
+}
+
+impl<T: constraints::ConstrainedUrl + Sync + Send + 'static> ToJSON for ConstrainedDiscordUrl<T> {
+    fn to_json(&self) -> Option<Value> {
+        self.0.to_json()
+    }
+}
+
+impl<T: constraints::ConstrainedUrl + Sync + Send + 'static> ParseFromJSON
+    for ConstrainedDiscordUrl<T>
+{
+    fn parse_from_json(value: Option<Value>) -> ParseResult<Self> {
+        let slf = DiscordUrl::parse_from_json(value).map_err(|e| e.propagate())?;
+
+        if !T::is_valid(&slf) {
+            Err(ParseError::custom("Invalid url provided."))
+        } else {
+            Ok(Self::from(slf))
+        }
+    }
+}
+
+impl<T: constraints::ConstrainedUrl + Sync + Send + 'static> FromStr for ConstrainedDiscordUrl<T> {
+    type Err = poem_openapi::types::ParseError<Self>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let slf = DiscordUrl::from_str(s).map_err(|e| e.propagate())?;
+
+        if !T::is_valid(&slf) {
+            Err(ParseError::custom("Invalid url provided."))
+        } else {
+            Ok(Self::from(slf))
+        }
+    }
+}
+
+impl<T: constraints::ConstrainedUrl> FromCqlVal<CqlValue> for ConstrainedDiscordUrl<T> {
+    fn from_cql(cql_val: CqlValue) -> Result<Self, FromCqlValError> {
+        let slf = DiscordUrl::from_cql(cql_val)?;
+
+        if !T::is_valid(&slf) {
+            Err(FromCqlValError::BadCqlType)
+        } else {
+            Ok(Self::from(slf))
+        }
+    }
+}
+
+impl<T: constraints::ConstrainedUrl> scylla::frame::value::Value for ConstrainedDiscordUrl<T> {
+    fn serialize(&self, buf: &mut Vec<u8>) -> Result<(), ValueTooBig> {
+        self.0.serialize(buf)
+    }
+}
 
 pub mod constraints {
-    use url::Url;
+    use crate::types::DiscordUrl;
 
     #[inline]
-    fn twitter_url(url: &Url) -> bool {
-        url.domain()
-            .map(|v| v == "twitter.com")
-            .unwrap_or_default()
+    fn twitter_url(url: &DiscordUrl) -> bool {
+        url.domain().map(|v| v == "twitter.com").unwrap_or_default()
     }
 
     #[inline]
-    fn github_url(url: &Url) -> bool {
-        url.domain()
-            .map(|v| v == "github.com")
-            .unwrap_or_default()
+    fn github_url(url: &DiscordUrl) -> bool {
+        url.domain().map(|v| v == "github.com").unwrap_or_default()
     }
 
     #[inline]
-    fn instagram_url(url: &Url) -> bool {
+    fn instagram_url(url: &DiscordUrl) -> bool {
         url.domain()
             .map(|v| v == "instagram.com")
             .unwrap_or_default()
     }
 
-
     pub trait ConstrainedUrl {
-        fn validate(url: &Url) -> bool;
+        fn is_valid(url: &DiscordUrl) -> bool;
     }
 
     macro_rules! constraint {
@@ -184,7 +273,7 @@ pub mod constraints {
             pub struct $name;
 
             impl $crate::types::url::constraints::ConstrainedUrl for $name {
-                fn validate(url: &Url) -> bool {
+                fn is_valid(url: &$crate::types::url::DiscordUrl) -> bool {
                     $crate::types::url::constraints::$cb(url)
                 }
             }
@@ -194,4 +283,91 @@ pub mod constraints {
     constraint!(TwitterUrl, twitter_url);
     constraint!(GitHubUrl, github_url);
     constraint!(InstagramUrl, instagram_url);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::types::url::constraints::{GitHubUrl, InstagramUrl, TwitterUrl};
+
+    #[test]
+    fn test_js_non_http_url() {
+        let res = DiscordUrl::from_str("javascript:alert(1)");
+        assert!(
+            res.is_err(),
+            "Expected url rejection for non http(s) protocols."
+        );
+    }
+
+    #[test]
+    fn test_mailto_non_http_url() {
+        let res = DiscordUrl::from_str("mailto:test@discordlist.gg");
+        assert!(
+            res.is_err(),
+            "Expected url rejection for non http(s) protocols."
+        );
+    }
+
+    #[test]
+    fn test_github_constrained_url_expect_ok() {
+        let res = ConstrainedDiscordUrl::<GitHubUrl>::from_str("https://github.com");
+        assert!(res.is_ok(), "Expected url pass for GitHubUrl urls.");
+    }
+
+    #[test]
+    fn test_github_constrained_url_expect_err() {
+        let res = ConstrainedDiscordUrl::<GitHubUrl>::from_str("mailto:test@discordlist.gg");
+        assert!(
+            res.is_err(),
+            "Expected url rejection for non GitHubUrl urls."
+        );
+
+        let res = ConstrainedDiscordUrl::<GitHubUrl>::from_str("https://discordlist.gg");
+        assert!(
+            res.is_err(),
+            "Expected url rejection for non GitHubUrl urls."
+        );
+    }
+
+    #[test]
+    fn test_twitter_constrained_url_expect_ok() {
+        let res = ConstrainedDiscordUrl::<TwitterUrl>::from_str("https://twitter.com");
+        assert!(res.is_ok(), "Expected url pass for TwitterUrl urls.");
+    }
+
+    #[test]
+    fn test_twitter_constrained_url_expect_err() {
+        let res = ConstrainedDiscordUrl::<TwitterUrl>::from_str("mailto:test@discordlist.gg");
+        assert!(
+            res.is_err(),
+            "Expected url rejection for non TwitterUrl urls."
+        );
+
+        let res = ConstrainedDiscordUrl::<TwitterUrl>::from_str("https://discordlist.gg");
+        assert!(
+            res.is_err(),
+            "Expected url rejection for non TwitterUrl urls."
+        );
+    }
+
+    #[test]
+    fn test_instagram_constrained_url_expect_ok() {
+        let res = ConstrainedDiscordUrl::<InstagramUrl>::from_str("https://instagram.com");
+        assert!(res.is_ok(), "Expected url pass for InstagramUrl urls.");
+    }
+
+    #[test]
+    fn test_instagram_constrained_url_expect_err() {
+        let res = ConstrainedDiscordUrl::<InstagramUrl>::from_str("mailto:test@discordlist.gg");
+        assert!(
+            res.is_err(),
+            "Expected url rejection for non InstagramUrl urls."
+        );
+
+        let res = ConstrainedDiscordUrl::<InstagramUrl>::from_str("https://discordlist.gg");
+        assert!(
+            res.is_err(),
+            "Expected url rejection for non InstagramUrl urls."
+        );
+    }
 }
